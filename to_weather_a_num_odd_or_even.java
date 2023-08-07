d83f6924464fb71d4fac0097273648cc1dd19b71
Stagnentwater <141637434+Stagnentwater@users.noreply.github.com>
2023-08-07 17:25:05 +0530

    Update to_weather_a_num_odd_or_even.java

--- to_weather_a_num_odd_or_even.java
+++ to_weather_a_num_odd_or_even.java
@@ -4,6 +4,11 @@ public class main{
   public static void main(string[] args){
     Scanner sc=new Scanner(System.in);
     int a=sc.nextInt;
+    if(a%2==0){
+      System.out.println("it's even")
+    }else{
+      System.out.println("it's odd")
+    }
     
   }
 
