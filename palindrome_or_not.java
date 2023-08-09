f5badf7f006a1b1d65d7ee9de1e1ac0ce3f863c3
Stagnentwater <141637434+Stagnentwater@users.noreply.github.com>
2023-08-09 17:13:03 +0530

    Update palindrome_or_not.java
    described the code for a better understanding at a later date

--- palindrome_or_not.java
+++ palindrome_or_not.java
@@ -3,11 +3,12 @@ public class Main {
     public static void main(String[] args) {
         Scanner sc=new Scanner(System.in);
         String A=sc.nextLine();
-        /* Enter your code here. Print output to STDOUT. */
+        //used a string builder as its fast and consumes less memory
         StringBuilder A_reversed = new StringBuilder();
         for(int i=A.length()-1;i>=0;i--){
             A_reversed.append(A.charAt(i));
         }
+        //used equals() also called reference comparision here as "==" only does pointer comparision not the content compare
         if (A_reversed.toString().equals(A)){
             System.out.println("Yes");
         }else{
