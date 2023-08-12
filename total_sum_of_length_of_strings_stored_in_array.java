b1b1bee33abc06c8d52a883f011b4d2555f01770
Stagnentwater <141637434+Stagnentwater@users.noreply.github.com>
2023-08-12 23:48:12 +0530

    Update total_sum_of_length_of_strings_stored_in_array.java
    added a faster way for this program using string builder class

--- total_sum_of_length_of_strings_stored_in_array.java
+++ total_sum_of_length_of_strings_stored_in_array.java
@@ -17,5 +17,23 @@ public class Main {
             totallength+=stringarray[i].length();
         }
         System.out.println(totallength);
+        // same thing with stringbuilder calss as follows
+
+        System.out.println("give the length of the array");
+        int x =sc.nextInt();
+        StringBuilder [] stringarray=new StringBuilder[x] ;
+        sc.nextLine();//eat up the newline value
+
+        for(int i=0;i<x;i++){
+            System.out.println("give an input string for:  "+(i+1)+"  index");
+            stringarray[i]=new StringBuilder("");
+            stringarray[i].append(sc.nextLine());
+        }
+        int totallength=0;
+        for(int i=0;i<x;i++){
+            totallength+=stringarray[i].length();
+        }
+        System.out.println(totallength);
     }
 }
+
